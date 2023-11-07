b7270224e45a7197de2aab8dd824cf01f27bfe80
Andrea Vouk <andrew.vouk@gmail.com>
2023-11-07 16:55:49 +0100

    Remove express 5 beta since high vulnerabilities are not fixed

--- src/routes/demo-route.ts
+++ src/routes/demo-route.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import demoController from '../controllers/demo-controller';
 import { validatedBody } from '../middlewares/validation';
 import { body } from 'express-validator';
@@ -12,12 +12,16 @@ interface ReqDemoRouteBody {
 router.post(
   '/',
   body('name').notEmpty().withMessage('value is null').isString().withMessage('value is not a string'),
-  async (req: Request, res: Response) => {
-    const body = validatedBody<ReqDemoRouteBody>(req);
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body = validatedBody<ReqDemoRouteBody>(req);
 
-    return res.status(200).json({
-      greetings: await demoController.greetings(body.name),
-    });
+      return res.status(200).json({
+        greetings: await demoController.greetings(body.name),
+      });
+    } catch (err) {
+      return next(err);
+    }
   },
 );
 
